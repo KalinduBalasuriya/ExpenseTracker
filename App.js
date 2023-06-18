a0497bdfa2af7a06e8d477a36ec0058fc22c0011
KalinduBalasuriya <61897592+KalinduBalasuriya@users.noreply.github.com>
2023-06-18 23:23:23 +0530

    up

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AllExpenses from './components/Screens/AllExpenses';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: '#252591' },
+          headerTintColor: 'white',
+          contentStyle: { backgroundColor: '#17186d' },
+          headerRight: () => {
+            return <Text>+</Text>
+          }
+        }} >
+          <Stack.Screen
+            name='All Expenses'
+            options={{ title: 'All Expenses' }}
+            component={AllExpenses}
+          >
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Screens/AllExpenses.js
@@ -0,0 +1,21 @@
+
+import { View, Text, StyleSheet } from "react-native";
+import Title from "../Title";
+
+function AllExpenses() {
+    return (
+        <View style={styles.container} >
+            <Title />
+        </View>
+    )
+}
+export default AllExpenses;
+
+const styles = StyleSheet.create({
+    container: {
+      
+    },
+
+})
+
+
